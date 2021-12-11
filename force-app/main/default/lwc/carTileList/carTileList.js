4057c8a64261c715792fd4cac4f5db7a832f2ec4
Moises <castillocarvajal.dev@gmail.com>
2021-12-11 12:48:42 -0600

    car tiles

--- force-app/main/default/lwc/carTileList/carTileList.js
+++ force-app/main/default/lwc/carTileList/carTileList.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getCars from '@salesforce/apex/CarController.getCars';
 
-export default class CarTileList extends LightningElement {}
+export default class CarTileList extends LightningElement {
+
+    cars
+    error
+    filters = {}
+
+    @wire(getCars, {filters: '$filters'})
+    carsHandler({data,error}){
+        if(data){
+            //console.log(data)
+            this.cars = data
+        }
+        if(error){
+            this.error = error
+            console.error(error)
+        }
+    }
+}
